85ab7d12a902b9aacf333099b441870c1a12ce93
Victor Grajski <victor.grajski@gmail.com>
2021-06-30 16:37:59 -0400

    basic remote connection working

--- public/index.js
+++ public/index.js
@@ -1,10 +1,9 @@
 // Making Connection
-const socket = io.connect("http://localhost:3000");
+const socket = io.connect("http://172.20.10.6:3000");
 socket.emit("joined");
 
 let players = []; // All players in the game
 let currentPlayer; // Player object for individual players
-let numPlayers = 0;
 
 class Player {
     constructor(id, x, y) {
@@ -25,7 +24,6 @@ document.getElementById("start-btn").addEventListener("click", () => {
 socket.on("join", (data) => {
     players.push(new Player(players.length, data.x, data.y));
 
-
     playersHTML = "";
     players.forEach((player) => {
         console.log(player);
@@ -37,8 +35,8 @@ socket.on("join", (data) => {
 socket.on("joined", (data) => {
     playersHTML = "";
     data.forEach((player, index) => {
-      players.push(new Player(index, player.name, player.pos, player.img));
-      console.log(player);
+      players.push(new Player(index, player.x, player.y));
+      console.log("meep");
       playersHTML += `<tr><td>${player.id}</td>`;
     });
     document.getElementById("players-table").innerHTML = playersHTML;
